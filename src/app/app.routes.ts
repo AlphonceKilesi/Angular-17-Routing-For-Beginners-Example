ef87387b49a2a6c4784883d112a8dfe7153b1e6c
Anil Singh <anil.singh581@gmail.com>
2024-02-28 18:07:39 +0530

    Add files via upload

---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+
+export const routes: Routes = [
+    {path:'', component:HomeComponent},
+    {path:'contactus', component:ContactusComponent},
+    {path:'aboutus', component:AboutusComponent}
+];
